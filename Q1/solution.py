68c728a5b72d02ba972d45e2a02f62eef5873768
amnasher <43099492+amnasher@users.noreply.github.com>
2022-09-19 13:38:41 +0500

    Update solution.py

--- Q1/solution.py
+++ Q1/solution.py
@@ -1,1 +1,21 @@
 ## Add code below with answer clearly stated
+
+def sumofdigits(number):
+    """Assumes n > 1.
+    returns sum of digits of n's factorial."""
+    fact = factorial(number)
+    print("Factorial",fact)
+    total = 0
+    for dig in str(fact):
+        total += int(dig)
+    return total
+
+
+def factorial(number):
+    if number <= 1:
+        return 1
+    return number * factorial(number - 1)
+
+
+if __name__ == '__main__':
+    print(sumofdigits(100))
